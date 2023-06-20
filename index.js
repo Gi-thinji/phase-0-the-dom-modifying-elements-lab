e3b3ecec0708407349b37924d10a4a82e82eeb90
Gi-thinji <githinji.ngare@gmail.com>
2023-06-20 21:53:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementsByTagName("body")[0]
+const childMain= main.querySelector("#main")
+main.remove(childMain)
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
+document.body.append(newHeader)
